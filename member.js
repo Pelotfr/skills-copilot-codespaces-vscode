448034dfca8888cb410efa6362fcbbad9e423986
Paul Meignan <65423129+Pelotfr@users.noreply.github.com>
2023-08-22 14:50:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/skills-member.html',
+    controller: 'SkillsMemberCtrl',
+    controllerAs: 'skillsMemberCtrl',
+    bindToController: true,
+    scope: {
+      member: '=',
+      skills: '='
+    }
+  };
+} 
